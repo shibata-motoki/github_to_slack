609fa1185de87aa69712fd9356716855edb4c99d
shibata-motoki <motoki.shibata@nex.nikkei.com>
2018-07-30 15:47:17 +0900

    ch all

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,14 @@
+# coding: UTF-8
+import requests
 import json
+post_url = 'https://hooks.slack.com/services/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'
 
 def lambda_handler(event, context):
-    # TODO implement
-    return { 'statusCode': 200, 'body': json.dumps(event) }
+     # slack送信Sample
+     requests.post(post_url, data = json.dumps({
+            'text': 'event{}\n\n\ncontext{}'.format(event, context), # 投稿するテキスト
+            'username': 'github', # 投稿のユーザー名
+            'link_names': 1, # メンションを有効にする
+            'channel': 'github_to_slack', # チャンネル
+            'icon_emoji': 'icon', # アイコン
+        }))
